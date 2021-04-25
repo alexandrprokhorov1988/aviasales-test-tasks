16ed9b8a2fff3c74fa1636f41fd329ce25b12a92
alexandrprokhorov1988 <alexandrprokhorov1988@gmail.com>
2021-04-25 22:49:06 +0300

    feat: добавить функции сортировки

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import './App.css';
-import TiketList from '../../components/TiketsList/TicketList';
+import TicketList from '../../components/TicketsList/TicketList';
 import data from '../../utils/data';
 
 function App() {
   const [tickets, setTickets] = React.useState([]);
   const [count, setCount] = React.useState(0);
-  const [buttonSortActive, setButtonSortActive] = React.useState(1);
+  const [buttonSortActive, setButtonSortActive] = React.useState('price');
   const lazyLoadRef = React.useRef();
 
   React.useEffect(() => {
-    setTickets(data.tickets);
+    setTickets(data.tickets.sort((a, b) => {
+      return a.price - b.price;
+    }));
   }, []);
 
   React.useEffect(() => {
@@ -29,16 +31,36 @@ function App() {
 
     let observer = new IntersectionObserver(callback, options);
     observer.observe(lazyLoadRef.current);
-  });
+  }, [count]);
 
-  function handleButtonSortClick(num) {
-    setButtonSortActive(+num);
+  function handleButtonSortClick(desc) {
+    setButtonSortActive(desc);
+    let func;
+    const priceSortFunc = (a, b) => a.price - b.price;
+    const durationSortFunc = (a, b) => {
+      return (a.segments[0].duration + a.segments[1].duration) -
+        (b.segments[0].duration + b.segments[1].duration);
+    };
+    const optimalSortFunc = (a, b) => {
+      const countStopsMOW = a.segments[0].stops.length + a.segments[1].stops.length;
+      const countStopsHKT = b.segments[0].stops.length + b.segments[1].stops.length;
+      if (a === b) {
+        return (a.segments[0].duration + a.segments[1].duration) -
+          (b.segments[0].duration + b.segments[1].duration);
+      }
+      return countStopsMOW - countStopsHKT;
+    };
+
+    if (desc === 'price') func = priceSortFunc;
+    if (desc === 'duration') func = durationSortFunc;
+    if (desc === 'optimal') func = optimalSortFunc;
+    setTickets(tickets.sort(func));
   }
 
   return (
     <div className="page">
       <main className="content">
-        <TiketList
+        <TicketList
           tickets={tickets}
           count={count}
           buttonActive={buttonSortActive}
